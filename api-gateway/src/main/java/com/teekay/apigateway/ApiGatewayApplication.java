d7eb77987ece2ba11872b4c65a647adae970ec5a
TK-MUKWEVHO <matodzikutama@gmail.com>
2023-03-28 14:15:17 +0200

    update

--- api-gateway/src/main/java/com/teekay/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/teekay/apigateway/ApiGatewayApplication.java
@@ -1,2 +1,13 @@
-package com.teekay.apigateway;public class ApiGatewayApplication {
+package com.teekay.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiGatewayApplication.class);
+    }
 }
---
+++ notificatin-service/src/main/java/org/teekay/NotificationServiceApplication.java
@@ -0,0 +1,2 @@
+package org.teekay;public class NotificationServiceApplication {
+}
